178ef00ba082af9d39219e3f11be3473b098de35
Rong "Mantle" Bao <mantlejonse@gmail.com>
2020-10-29 07:05:29 +0800

    resource: workaround

--- assets/js/xs-school-mainpage.js
+++ assets/js/xs-school-mainpage.js
@@ -1,3 +1,12 @@
+(function() {
+  const body = document.getElementsByTagName("body")[0]
+  const jqueryScript = document.createElement("script")
+  jqueryScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
+  jqueryScript.setAttribute("integrity", "sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==")
+  jqueryScript.setAttribute("crossorigin", "anonymous")
+  body.insertAdjacentElement("afterbegin", jqueryScript)
+})();
+
 const ELEMID_PRE_USER_AGENT = 'pre-user-agent'
 const ELEMID_FIGURE_UNDER_CONSTRUCTION = 'figure-under-construction'
 const ELEMID_H2_DEPRECATED_BANNER = 'h2-deprecated-banner'
