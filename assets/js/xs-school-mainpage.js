822e9ffadaba69252af7aa520ac86084b648206f
Rong "Mantle" Bao <mantlejonse@gmail.com>
2020-11-02 17:28:42 +0800

    resource: add CustomEase3 and CustomWiggle3

--- assets/js/xs-school-mainpage.js
+++ assets/js/xs-school-mainpage.js
@@ -1,6 +1,7 @@
 (function() {
-  // HACK: bypass script tag filtering
+  // HACK: bypass script tag filtering, inserting tags for script, one linked by one
   const body = document.getElementsByTagName('body')[0]
+  
   const jqueryScript = document.createElement('script')
   jqueryScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js')
   jqueryScript.setAttribute('integrity', 'sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==')
@@ -11,7 +12,26 @@
   prefixfreeScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js')
   prefixfreeScript.setAttribute('integrity', 'sha512-p7Ey2nBhKYEi9yh0iDs+GMA0ttebOqVl3OO2oWRzRxtDoN/RedyYcHFUJZhMVi8NKRdEA7n+9NTNQX/kFIZgNw==')
   prefixfreeScript.setAttribute('crossorigin', 'anonymous')
-  body.insertAdjacentElement('afterbegin', prefixfreeScript)
+  jqueryScript.insertAdjacentElement('afterend', prefixfreeScript)
+  
+  const gsapScript = document.createElement('script')
+  gsapScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js')
+  gsapScript.setAttribute('integrity', 'sha512-IQLehpLoVS4fNzl7IfH8Iowfm5+RiMGtHykgZJl9AWMgqx0AmJ6cRWcB+GaGVtIsnC4voMfm8f2vwtY+6oPjpQ==')
+  gsapScript.setAttribute('crossorigin', 'anonymous')
+  gsapScript.addEventListener('load', () => {
+    const customEaseScript = document.createElement('script')
+    customEaseScript.setAttribute('src', 'https://cdn.jsdelivr.net/gh/CSharperMantle/CSharperMantle.github.io@HEAD/assets/js/CustomEase-3.5.1.min.js')
+    gsapScript.insertAdjacentElement('afterend', customEaseScript)  
+  
+    const customWiggleScript = document.createElement('script')
+    customWiggleScript.setAttribute('src', 'https://cdn.jsdelivr.net/gh/CSharperMantle/CSharperMantle.github.io@HEAD/assets/js/CustomWiggle-3.4.3.min.js')
+    customWiggleScript.addEventListener('load', () => {
+      loadDropper()
+    })
+    customEaseScript.insertAdjacentElement('afterend', customWiggleScript)
+  })
+  prefixfreeScript.insertAdjacentElement('afterend', gsapScript)
+  
 })()
 
 const ELEMID_PRE_USER_AGENT = 'pre-user-agent'
@@ -278,6 +298,265 @@ let isDeprecatedBannerShowing = false;
     }, SLIDER_SPEED)
   }
 })();
+
+// gsap animation
+// TODO: eslint this code
+function loadDropper() {
+  let select = (s) => document.querySelector(s),
+    selectAll = (s) => document.querySelectorAll(s),
+    mainSVG = select(".mainSVG"),
+    liquid = select("#liquid"),
+    pointArray = [],
+    pointValueXArray = [],
+    pointValueYArray = [],
+    liquidWidth = 300,
+    numPoints = 30,
+    dripOffset = 0.0258,
+    rippleDuration = 4.6,
+    rippleAmount = "+=8",
+    startValX = 250,
+    startValY = 400,
+    colorArray = [
+      "#E6098B",
+      "#FFBE0B",
+      "#FB5607",
+      "#8338EC",
+      "#3A86FF",
+      "#51E5FF",
+      "#04A777",
+      "#F75C03",
+      "#F71735"
+    ],
+    allBubbles = selectAll("#bubbleGroup rect");
+
+  gsap.set(mainSVG, {
+    visibility: "visible"
+  });
+  gsap.set(".darkLiquid", {
+    scaleX: -1,
+    transformOrigin: "50% 50%"
+  });
+  for (let i = 0; i < numPoints; i++) {
+    let p = liquid.points.appendItem(mainSVG.createSVGPoint());
+    pointArray.push(p);
+    pointValueXArray.push(
+      i < numPoints - 2 ? startValX : i == numPoints - 2 ? 600 : 200
+    );
+    startValX += liquidWidth / (numPoints - 2);
+    pointValueYArray.push(i < numPoints - 2 ? startValY : 800);
+  }
+
+  gsap.set(pointArray, {
+    x: gsap.utils.wrap(pointValueXArray),
+    y: gsap.utils.wrap(pointValueYArray)
+  });
+
+  gsap.set("#level", {
+    transformOrigin: "50% 100%"
+  });
+  gsap.set("#bubbleGroup rect, #droplet", {
+    transformOrigin: "50% 50%"
+  });
+  gsap
+    .fromTo(
+      allBubbles,
+      {
+        x: "random(0, 200)",
+        y: "random(0, 120)",
+        scale: "random(0.5, 3)",
+        rotation: "random(20, 180)",
+        opacity: 1
+      },
+      {
+        duration: 1,
+        rotation: "random(180, 360)",
+        repeatRefresh: true,
+        stagger: {
+          each: 0.52,
+          repeat: -1
+        },
+        scale: 0.1,
+        y: "-=30",
+        opacity: 0.1
+      }
+    )
+    .seek(100);
+
+  const makeDrip = () => {
+    let currentColor = gsap.utils.random(colorArray);
+    gsap.to(":root", { "--main-color": currentColor });
+
+    let tl = gsap.timeline({
+      defaults: {
+        ease: CustomWiggle.create("", {
+          type: "easeOut",
+          wiggles: gsap.utils.random(9, 12)
+        })
+      }
+    });
+
+    tl.fromTo(
+      "#pipette1",
+      {
+        x: 600,
+        opacity: 0
+      },
+      {
+        duration: 1,
+        x: 376,
+        opacity: 1,
+        ease: "expo.inOut"
+      }
+    )
+      .fromTo(
+        "#pipette1",
+        {
+          rotation: -95,
+          transformOrigin: "50% 100%"
+        },
+        {
+          rotation: 0,
+          transformOrigin: "50% 100%",
+          duration: 1.5,
+          ease: "elastic(1.5, 0.83)"
+        },
+        0
+      )
+      .addLabel("pipetteReady")
+      .fromTo(
+        "#drip",
+        {
+          scale: 0
+        },
+        {
+          duration: 1,
+          scale: 1,
+          transformOrigin: "50% 0%",
+          ease: "elastic(1, 0.8)"
+        }
+      )
+      .to(
+        "#level",
+        {
+          duration: 1,
+          scaleY: 0.5,
+          ease: "elastic(1, 0.8)"
+        },
+        "pipetteReady"
+      )
+      .fromTo(
+        "#drip",
+        {
+          x: 399,
+          y: 155
+        },
+        {
+          x: 399,
+          y: 430,
+          duration: 0.38,
+          ease: "power1.in"
+        }
+      )
+      .addLabel("splash")
+      .to(
+        ".poly",
+        {
+          fill: currentColor,
+          ease: "sine"
+        },
+        "splash"
+      )
+      .to(
+        "#bubbleGroup",
+        {
+          stroke: currentColor,
+          ease: "sine"
+        },
+        "splash"
+      )
+      .to(
+        pointArray,
+        {
+          duration: gsap.utils.random(3, 5),
+          y: (i) => {
+            return rippleAmount;
+          },
+          stagger: {
+            each: dripOffset,
+            from: "center"
+          }
+        },
+        "splash"
+      )
+      .to(
+        "#bubbleGroup",
+        {
+          duration: 4,
+          y: "+=random(5, 10)",
+          ease: "wiggle({type:easeOut, wiggles:10})"
+        },
+        "splash"
+      )
+      .to(
+        "#droplet",
+        {
+          duration: 0.23,
+          y: "random(-30, -60, 1)",
+          rotation: "random(20, 290)",
+          ease: "power1"
+        },
+        "splash"
+      )
+      .to(
+        "#droplet",
+        {
+          duration: 0.23,
+          y: 0,
+          rotation: "+=30",
+          ease: "power1.in"
+        },
+        "splash+=0.23"
+      )
+      .fromTo(
+        "#droplet",
+        {
+          scale: 1
+        },
+        {
+          duration: 0.23,
+          scale: 0,
+          transformOrigin: "50% 100%",
+          ease: "expo.in"
+        },
+        "splash+=0.23"
+      )
+      .to(
+        "#level",
+        {
+          duration: 1,
+          scaleY: 1,
+          ease: "expo.in"
+        },
+        "splash"
+      )
+      .to(
+        "#pipette1",
+        {
+          duration: 1,
+          rotation: 23,
+          x: 100,
+          opacity: 0,
+          ease: "expo.in"
+        },
+        "splash"
+      );
+
+    gsap.delayedCall(4, makeDrip);
+  };
+
+  makeDrip();
+};
+
 
 // other interactive elements
 (function() {
