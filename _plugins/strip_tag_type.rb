7d10a25129dd05b7896e8b7bb8b2156a056fa12d
Rong "Mantle" Bao <baorong2005@126.com>
2025-01-28 10:44:19 +0800

    add topic tags

--- _plugins/strip_tag_type.rb
+++ _plugins/strip_tag_type.rb
@@ -1,8 +1,7 @@
 module Jekyll
   module StripTopicFilter
     def strip_topic(input)
-      return [] if input.nil?
-      input.map { |tag| tag.sub(/^[^:]+:/, '') }
+      input ? input.map { |tag| tag.sub(/^[^:]+:/, '') } : []
     end
   end
 end
