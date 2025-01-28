380b9ac3f9f540adc6664d9a7e542495986c230a
Rong "Mantle" Bao <baorong2005@126.com>
2025-01-28 17:31:33 +0800

    add tag index

--- _plugins/strip_tag_type.rb
+++ _plugins/strip_tag_type.rb
@@ -1,9 +1,12 @@
 module Jekyll
-  module StripTopicFilter
-    def strip_topic(input)
-      input ? input.map { |tag| tag.sub(/^[^:]+:/, '') } : []
+  module StripTagTypeFilter
+    def strip_tag_type(input)
+      (!input.nil? && input.kind_of?(String)) ? input.sub(/^[^:]+:/, '') : input
+    end
+    def strip_tag_type_each(input)
+      (!input.nil? && input.kind_of?(Array)) ? input.map { |tag| tag.sub(/^[^:]+:/, '') } : []
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::StripTopicFilter)
+Liquid::Template.register_filter(Jekyll::StripTagTypeFilter)
